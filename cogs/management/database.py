013949f0b3b4f2965ea823473a352be57c65ede0
MrRazamataz <mrrazamataz@mrrazamataz.ga>
2022-06-04 18:59:38 +0100

    starting

---
+++ cogs/management/database.py
@@ -0,0 +1,60 @@
+# MrRazamataz's RazBot
+# database man cog
+import yaml
+from discord.ext import commands
+import aiofiles
+import aiomysql
+
+
+
+class db(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    '''
+        @commands.Cog.listener()
+        async def on_guild_join(self, guild):
+            async with aiosqlite.connect("database.db") as db:
+                await db.execute(f"INSERT INTO guild_settings (id, member_count) VALUES ({guild.id}, {guild.member_count})")
+                await db.commit()
+    '''
+    @commands.Cog.listener()
+    async def on_ready(self):
+        global tokens
+        with open("token.yml", mode="r") as file:
+            tokens = yaml.safe_load(file)
+        print("[DB] Connecting to DB...")
+        pool = await aiomysql.create_pool(host=tokens["database_info"]["host"], port=3306,
+                                          user=tokens["database_info"]["username"], password=tokens["database_info"]["password"],
+                                          db='razbotxy_botDB')
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                # await cur.execute(create_table_query)
+                await cur.execute('CREATE TABLE IF NOT EXISTS guild_settings (id BIGINT, member_count INT, apod_channel BIGINT)')
+                for guild in self.bot.guilds:
+                    print(f"[DB] [Guild settings] Checking for settings in `{guild.id}`...")
+                    check = await cur.execute(f"SELECT member_count FROM guild_settings WHERE id = {guild.id}")
+                    if check is None:
+                        try:
+                            await cur.execute(
+                                f"INSERT INTO guild_settings (id, member_count) VALUES ({guild.id}, {guild.member_count})")
+                            await conn.commit()
+                        except Exception as e:
+                            print(f"Error with {guild.id}! \n{e}")
+                            pass
+                    else:
+                        await cur.execute(
+                            f"UPDATE guild_settings SET member_count = {guild.member_count} WHERE id = {guild.id}")
+                        await conn.commit()
+
+        conn.close()
+        pool.close()
+        await pool.wait_closed()
+        print("[DB] Connection closed.")
+
+
+
+
+
+
+async def setup(bot):
+    await bot.add_cog(db(bot))
--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 # RazBot new boy edition
+import asyncio
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 import yaml
 from discord import app_commands
+cogs = ['cogs.management.database']
+print("Imported libs. RazBot is starting...")
 
 with open("config.yml", 'r') as yaml_read:
     config = yaml.safe_load(yaml_read)
 with open("token.yml", 'r') as yaml_read:
     token = yaml.safe_load(yaml_read)
+
 description = '''This is the RazBot rewrite.'''
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix='!', description=description, intents=intents)
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 
-@client.event
+
+async def main():
+    async with bot:
+        for cog in cogs:  # Looks for the cogs,
+            await bot.load_extension(cog)  # Loads the cogs.
+        try:
+            await bot.start(token["secret_stuff"]["token"])
+        except KeyboardInterrupt:
+            print("Bye!")
+            await bot.close()
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
 
-client.run(token["secret_stuff"]["token"])
+
+
+asyncio.run(main())
