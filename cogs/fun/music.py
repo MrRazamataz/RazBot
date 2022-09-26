46f4401be9d7f375be8df55ce70871a41293b63f
MrRazamataz <mrrazamataz@mrrazamataz.ga>
2022-09-26 22:56:58 +0100

    start on music

--- cogs/fun/music.py
+++ cogs/fun/music.py
@@ -7,11 +7,54 @@ from discord import app_commands
 from discord.app_commands import Choice
 import datetime
 import wavelink
+import re
+
+URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+
+
+class QueueIsEmpty(commands.CommandError):
+    pass
+
+
+class NoVoiceChannel(commands.CommandError):
+    pass
 
 
 class music(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
+        bot.loop.create_task(self.connect_nodes())
+
+    async def connect_nodes(self):
+        """Connect to our Lavalink nodes."""
+        await self.bot.wait_until_ready()
+
+        await wavelink.NodePool.create_node(bot=self.bot,
+                                            host='65.21.143.114',
+                                            port=2333,
+                                            password='youshallnotpass')
+
+    @commands.Cog.listener()
+    async def on_wavelink_node_ready(self, node: wavelink.Node):
+        """Event fired when a node has finished connecting."""
+        print(f'Node: <{node.identifier}> is ready!')
+
+    @commands.hybrid_command(name="play", aliases=["p"])
+    async def play_command(self, ctx, *, query: wavelink.YouTubeTrack) -> None:
+        """Play a song."""
+        if not ctx.voice_client:
+            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        else:
+            vc: wavelink.Player = ctx.voice_client
+
+        await vc.play(query)
+
+    @play_command.error
+    async def play_command_error(self, ctx, exc):
+        if isinstance(exc, QueueIsEmpty):
+            await ctx.send("[!] No songs to play as the queue is empty.")
+        elif isinstance(exc, NoVoiceChannel):
+            await ctx.send("[!] No suitable voice channel was provided.")
 
 
 async def setup(bot: commands.Bot) -> None:
